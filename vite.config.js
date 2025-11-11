23d95e6fbc027406638868158fe9d70dede91aeb
Jamshid <jamshidqayimov0399@gmail.com>
2025-11-11 12:45:38 +0500

    start

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: '.',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            input: {
+                main: './index.html'
+            }
+        },
+        css: {
+            devSourcemap: true
+        }
+    },
+    server: {
+        port: 3000,
+        open: true
+    },
+
+});
